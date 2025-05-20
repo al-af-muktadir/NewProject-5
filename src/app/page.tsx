4a86e0d1d5bf0d5fd3638ccaebc3ec7da96ab50b
al-af-muktadir <al.afmuktadir.28@gmail.com>
2025-05-20 23:37:37 +0600

    hocche

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,7 @@ import Skills from "@/Components/Skills";
 import React from "react";
 
 const Home = () => {
+  console.log("inhome", process.env.NEXT_PUBLIC_BACKEND_API);
   return (
     <div>
       <Body />
